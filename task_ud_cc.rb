18cd9a10f0a8fa9cd4dd1f859b08c2b093217c02
Yiang Gan <getbox@126.com>
2016-01-13 14:45:49 +0800

    fix connect

--- task_ud_cc.rb
+++ task_ud_cc.rb
@@ -2,7 +2,7 @@ require 'sequel'
 
 #usr@pwd may cause error if pwd has '@';
 git_tree="--git-dir=/home/git/repositories"
-DB = Sequel.connect('mysql2://127.0.0.1:3306/gitlab?characterEncoding=UTF-8')
+DB = Sequel.connect('mysql2://127.0.0.1:3306/gitlab?user=usr&password=pwd&characterEncoding=UTF-8')
 
 DB[:projects].select(:id,:name,:namespace_id).each  do |row|
   namespace=DB[:namespaces].select(:name).where(:id=>row.fetch(:namespace_id)).to_hash(nil,:name)

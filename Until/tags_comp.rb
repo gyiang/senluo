c53040f41b2505c886b41cee02514f849603d27c
Yiang Gan <getbox@126.com>
2016-01-31 03:43:14 +0800

    match_tag:save into db

--- Until/tags_comp.rb
+++ Until/tags_comp.rb
@@ -2,6 +2,12 @@ require 'sequel'
 
 DB=Sequel.connect('mysql2://root:1234@127.0.0.1:3306/neo?characterEncoding=UTF-8')
 
+DB.create_table?(:proj_tags,:charset => 'utf8') do
+  Integer :projid
+  String :tags
+end
+
+
 h={} #77757 elements
 DB[:open_source_projects].select(:id).each do |row|
   DB[:proj_tag].select().where(:projId=>row[:id]).each do |tag|
@@ -14,33 +20,46 @@ DB[:open_source_projects].select(:id).each do |row|
   p row[:id]
 end
 
+is_save=false
+if is_save then
+  # save
+  tags=DB[:proj_tags]
+  tag={}
+  h.each_pair do |k,v|
+    tag['projid']=k.to_i
+    tag['tags']=v.to_s
+    tags.insert(tag)
+  end
+end
 
-File.new("/root/Desktop/h","a+").puts(h)
-
+DB.create_table?(:values,:charset => 'utf8') do
+  Integer :k1
+  Integer :k2
+  Integer :v1_len
+  Integer :v2_len
+  Integer :c
+  Double  :similarity
+end
+values=DB[:values]
+value={}
 
 h.each_pair do |k1,v1|
   h.each_pair do |k2,v2|
     if k2.to_i< k1.to_i then next end
-    p [k1,k2,(v1 & v2).size]
-    resulet.puts("#{k1},#{k2},#{(v1 & v2).size}\n")
-  end
-end
+    c=(v1 & v2).size
+    if c==0 then next end
 
-value=[]
-=begin
-10.times do |x|
-  x=x+1
-  if h[x] then
-    for y in x..10 do
-       (h[x] & h[y]).size
-    #65
-    end
-  else
-    for y in x..10 do
-      value[x][y]=value[y][x]= 0
-    end
+    v1_len=v1.length
+    v2_len=v2.length
+    value['k1']=k1
+    value['k2']=k2
+    value['v1_len']=v1_len
+    value['v2_len']=v2_len
+    value['c']=c
+    #value['similarity']=c/Float(v1_len**(1.0/2)*v2_len**(1.0/2))
+    value['similarity']=c/(Math.sqrt(v1_len)*Math.sqrt(v2_len))
+    values.insert(value)
+    #resulet.puts("#{k1},#{k2},#{(v1 & v2).size}\n")
   end
+  p "#{k1}/#{h.size}"
 end
-=end
-
-p value
